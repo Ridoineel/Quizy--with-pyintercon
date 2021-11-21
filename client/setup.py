e3656ebe15ae541a579455a0fc3ce18f99988f28
ridoineel <ridoineelofficiel@gmail.com>
2021-11-21 09:05:06 +0000

    Update of client/setup.py and add Color

--- client/setup.py
+++ client/setup.py
@@ -7,6 +7,10 @@ from pyintercon import Client
 host = "localhost"
 port  = 8080
 
+class Color:
+	success = lambda _str: '\033[92m' + _str + '\033[0m'
+	danger = lambda _str: '\033[91m' + _str + '\033[0m'
+
 def getQuestions(cl, nb):
     """ return question id and question label
 
@@ -44,7 +48,7 @@ def main():
     cl.connect(host, port)
 
     nb_random_questions = 10
-
+    
     print("QUIZY".center(40))
     print()
 
@@ -54,15 +58,24 @@ def main():
 
     i = 1
     for id, question in questions:
-        print(f"{i}: {question}")
+        print(f"Q{i}: {question}")
 
-        answer = input("Your answer: ")
+        answer = input(">>> ")
 
         success = submit(cl, id, answer)
 
-        quiz_results.append(success)
+        if success:
+            success_msg = Color.success("Right answer")
+        else:
+            success_msg = Color.danger("Wrong answer")
 
-        system("clear")
+        print(success_msg)
+
+        quiz_results.append(["Wrong", "Right"][success])
+
+        # system("clear")
+        print()
+
         i += 1
 
     print(quiz_results)
