f2ace59e12b2d724e5ac2558b24e0024fac22c65
ridoineel <ridoineelofficiel@gmail.com>
2022-04-15 19:49:02 +0000

    update server utils functions

--- server/utils/variables.py
+++ server/utils/variables.py
@@ -1,12 +1,13 @@
 import re
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, dirname
 
+_parentdir = dirname(dirname(__file__))
 # quizs paths
 quizs_path = list()
 
-for file in listdir("datas"):
-	file_path = join("datas", file)
+for file in listdir(join(_parentdir, "datas")):
+	file_path = join(_parentdir, "datas", file)
 
 	# filter the json files
 	if isfile(file_path) and re.match(r"^.*\.json$", file) is not None:
